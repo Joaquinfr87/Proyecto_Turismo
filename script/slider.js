99e7c1c47e4f302f87aab5251db32c236d5e9b6f
Joaquinfr87 <vader87joaquin@gmail.com>
2024-09-02 11:42:12 -0400

    Cambios slider

--- script/slider.js
+++ script/slider.js
@@ -1,12 +1,12 @@
-let next = document.querySelector(".next")
-let prev = document.querySelector(".prev")
+// let next = document.querySelector(".next")
+// let prev = document.querySelector(".prev")
 
-next.addEventListener("click", function(){
-    let items = document.querySelectorAll(".item")
-    document.querySelector(".slide").appendChild(items[0])
-})
+// next.addEventListener("click", function(){
+//     let items = document.querySelectorAll(".item")
+//     document.querySelector(".slide").appendChild(items[0])
+// })
 
-prev.addEventListener("click", function(){
-    let items = document.querySelectorAll(".item")
-    document.querySelector(".slide").prepend(items[items.length - 1])
-})
+// prev.addEventListener("click", function(){
+//     let items = document.querySelectorAll(".item")
+//     document.querySelector(".slide").prepend(items[items.length - 1])
+// })
